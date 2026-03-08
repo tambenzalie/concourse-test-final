9a3563009c04e85ba2de0f384cde2a56a7201be7
howard.nzalie <howard.tambe@outlook.com>
2026-03-08 15:05:21 +0000

    Fresh start

---
+++ ci/test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "---------------------------------------"
+echo "Checking JJTECHEc2-Repo resources..."
+echo "Current directory: $(date)"
+echo "Files found:"
+ls -R
+echo "---------------------------------------"
