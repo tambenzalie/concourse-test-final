e2f6eb124efaff1877f933b1fdff6655e7860c89
howard.nzalie <howard.tambe@outlook.com>
2026-03-08 15:36:20 +0000

    Testing automatic trigger

--- ci/test.sh
+++ ci/test.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-echo "---------------------------------------"
-echo "Checking JJTECHEc2-Repo resources..."
-echo "Current directory: $(date)"
-echo "Files found:"
-ls -R
-echo "---------------------------------------"
+#!/bin/sh
+echo "Hello from my updated Concourse script!"
+echo "The time is $(date)"
